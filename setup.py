0b9910ad62ec52f33505e3ed578bcfa0c990ca38
Burbon13 <razvanroatis91@gmail.com>
2020-10-07 00:27:18 +0200

    First CLI commands

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='my_funny_script',
+    version='0.1',
+    py_modules=['terminal'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        silly=src.scripts.silly:cli
+    ''',
+)
