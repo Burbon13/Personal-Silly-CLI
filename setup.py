0d07d9c14cf084f19668cae62d49ce00eb6d5282
Burbon13 <razvanroatis91@gmail.com>
2020-10-07 21:21:55 +0200

    Small additions

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='my_funny_script',
+    name='my_silly_cli_tool',
     version='0.1',
+    author='Razvan Roatis',
     py_modules=['terminal'],
+    description='A set of pretty silly personal CLI tasks',
     install_requires=[
         'Click',
     ],
